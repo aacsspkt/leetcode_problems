d5cda67dd442ab46bceb5dc742d9d731604d2612
Ashish Sapkota <aacsspkt3@gmail.com>
2024-02-06 23:15:27 +0545

    solve roman to integer problem

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ pub mod two_sum;
 pub mod add_two_numbers;
 pub mod longest_substring_length;
 pub mod palindrome_number;
+pub mod roman_to_integer;
+
 
 pub struct Solution;
 
---
+++ src/roman_to_integer.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+
+use crate::Solution;
+
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        let mut numbers = HashMap::from([
+            ('I', 1),
+            ('V', 5),
+            ('X', 10),
+            ('L', 50),
+            ('C', 100),
+            ('D', 500),
+            ('M', 1000),
+        ]);
+
+        let mut sum = 0;
+        let mut highest_value = 0;
+        // MCMXCIV
+
+        for ch in s.chars().rev() {
+            let &value = numbers.get(&ch).unwrap();
+
+            match value.cmp(&highest_value) {
+                std::cmp::Ordering::Greater => {
+                    sum += value;
+                    highest_value = value;
+                }
+                std::cmp::Ordering::Equal => sum += value,
+                std::cmp::Ordering::Less => sum -= value,
+            }
+        }
+        sum
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works_1() {
+        let s = "III".to_string();
+        let i = Solution::roman_to_int(s);
+        assert_eq!(i, 3);
+
+        let s = "LVIII".to_string();
+        let i = Solution::roman_to_int(s);
+        assert_eq!(i, 58);
+
+        let s = "MCMXCIV".to_string();
+        let i = Solution::roman_to_int(s);
+        assert_eq!(i, 1994);
+
+        let s = "MMMCMXCIX".to_string();
+        let i = Solution::roman_to_int(s);
+        assert_eq!(i, 3999);
+    }
+}
